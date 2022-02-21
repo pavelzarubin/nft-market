dfb334da8917cbf042f842b05550e2eaeb984332
Paul <pavel.zarubin@metalamp.io>
2022-02-21 16:34:03 +0300

    working nft market, trace

--- src/NFTSale.hs
+++ src/NFTSale.hs
@@ -3,7 +3,9 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NumericUnderscores #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeApplications #-}
@@ -11,29 +13,39 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 
-module NFTSale where
+module NFTSale (saleEndpoints, NFT (..), StartSaleParams (..)) where
 
 import Control.Monad hiding (fmap)
 import Data.Aeson (FromJSON, ToJSON)
 import qualified Data.Map as Map
---import qualified Data.OpenApi as OpenApi
 import Data.Text (Text)
 import GHC.Generics (Generic)
 import Ledger hiding (singleton)
 import Ledger.Ada as Ada
 import Ledger.Constraints as Constraints
 import qualified Ledger.Typed.Scripts as Scripts
+import Ledger.Value as Value
 import Playground.Contract (ToSchema)
 import Plutus.Contract as Contract
 import qualified PlutusTx
 import PlutusTx.Prelude hiding (Semigroup (..), unless)
-import Prelude (Eq, Semigroup (..), Show (..), String, last)
+import Prelude (Semigroup (..), Show (..), String)
+
+minLovelace :: Value
+minLovelace = lovelaceValueOf 2_000_000
+
+getLovelaceFromValueOf :: Value -> Integer
+getLovelaceFromValueOf = getLovelace . fromValue
 
 data NFT = NFT
   { nftTokenName :: !TokenName,
     nftCurrencySymbol :: !CurrencySymbol
   }
-  deriving (Show, Generic, FromJSON, ToJSON)
+  deriving (Show, Generic, FromJSON, ToJSON, ToSchema)
+
+instance Eq NFT where
+  {-# INLINEABLE (==) #-}
+  NFT t1 c1 == NFT t2 c2 = (t1 == t2) && (c1 == c2)
 
 PlutusTx.unstableMakeIsData ''NFT
 
@@ -51,4 +63,126 @@ data NFTSaleRedeemer = Close | Buy deriving (Show, Generic, FromJSON, ToJSON)
 PlutusTx.unstableMakeIsData ''NFTSaleRedeemer
 
 mkNFTSaleValidator :: NFTSale -> NFTSaleRedeemer -> ScriptContext -> Bool
-mkNFTSaleValidator saleParams red ctx = True
+mkNFTSaleValidator NFTSale {..} red ctx =
+  True
+    && case red of
+      Close -> traceIfFalse "must be signed by seller" signedBySeller
+      Buy -> traceIfFalse "low price" checkPrice
+  where
+    info :: TxInfo
+    info = scriptContextTxInfo ctx
+
+    pkh :: PubKeyHash
+    pkh = unPaymentPubKeyHash nsSeller
+
+    signedBySeller :: Bool
+    signedBySeller = txSignedBy info pkh
+
+    checkPrice :: Bool
+    checkPrice = getLovelaceFromValueOf (valuePaidTo info pkh) >= (nsPrice + 2_000_000)
+
+data Sale
+
+instance Scripts.ValidatorTypes Sale where
+  type DatumType Sale = NFTSale
+  type RedeemerType Sale = NFTSaleRedeemer
+
+typedValidator :: Scripts.TypedValidator Sale
+typedValidator =
+  Scripts.mkTypedValidator @Sale
+    $$(PlutusTx.compile [||mkNFTSaleValidator||])
+    $$(PlutusTx.compile [||wrap||])
+  where
+    wrap = Scripts.wrapValidator @NFTSale @NFTSaleRedeemer
+
+saleValidator :: Validator
+saleValidator = Scripts.validatorScript typedValidator
+
+saleAddress :: Address
+saleAddress = scriptAddress saleValidator
+
+-----------------------------------------------------------------------
+
+data StartSaleParams = StartSaleParams
+  { sspNFT :: NFT,
+    sspPrice :: Integer
+  }
+  deriving (Show, Generic, FromJSON, ToJSON, ToSchema)
+
+startSale :: StartSaleParams -> Contract w NFTSaleSchema Text ()
+startSale (StartSaleParams nft price) = do
+  pkh <- Contract.ownPaymentPubKeyHash
+  let dat =
+        NFTSale
+          { nsSeller = pkh,
+            nsPrice = price,
+            nsNFT = nft
+          }
+      val = Value.singleton (nftCurrencySymbol nft) (nftTokenName nft) 1 <> minLovelace
+      tx = (Constraints.mustPayToTheScript dat val)
+  ledgerTx <- submitTxConstraints typedValidator tx
+  awaitTxConfirmed $ getCardanoTxId ledgerTx
+  logInfo @String $ "Sale of " ++ (show nft) ++ " started for price = " ++ (show price)
+  return ()
+
+findNFTSale ::
+  NFT ->
+  Map.Map TxOutRef ChainIndexTxOut ->
+  Maybe (TxOutRef, NFTSale)
+findNFTSale nft@NFT {..} utxos = h g
+  where
+    f = \ch -> (valueOf (_ciTxOutValue ch) nftCurrencySymbol nftTokenName) == 1
+    g = Map.toList $ Map.filter f utxos
+    h ((oref', ch') : xs) = case _ciTxOutDatum ch' of
+      Left _ -> h xs
+      Right (Datum d) -> case PlutusTx.fromBuiltinData d :: Maybe NFTSale of
+        Nothing -> h xs
+        (Just nftSale) -> if nft == (nsNFT nftSale) then Just (oref', nftSale) else Nothing
+    h _ = Nothing
+
+closeSale :: NFT -> Contract w NFTSaleSchema Text ()
+closeSale nft = do
+  utxos <- utxosAt saleAddress
+  let tst = findNFTSale nft utxos
+  case tst of
+    Just (oref, _) -> do
+      logInfo @String "Sale founded"
+      let lookups =
+            Constraints.unspentOutputs utxos
+              <> Constraints.otherScript saleValidator
+          tx = (Constraints.mustSpendScriptOutput oref $ Redeemer $ PlutusTx.toBuiltinData Close)
+      ledgerTx <- submitTxConstraintsWith @Sale lookups tx
+      void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
+      logInfo @String "Sale closed"
+    _ -> logError @String $ "Sale not founded. Datums: " ++ (show tst)
+
+buyNFT :: NFT -> Contract w NFTSaleSchema Text ()
+buyNFT nft = do
+  utxos <- utxosAt saleAddress
+  let tst = findNFTSale nft utxos
+  case tst of
+    Just (oref, NFTSale {..}) -> do
+      logInfo @String "Sale founded"
+      let lookups =
+            Constraints.unspentOutputs utxos
+              <> Constraints.otherScript saleValidator
+          ada = lovelaceValueOf nsPrice <> minLovelace
+          tx =
+            (Constraints.mustSpendScriptOutput oref $ Redeemer $ PlutusTx.toBuiltinData Buy)
+              <> (Constraints.mustPayToPubKey nsSeller ada)
+      ledgerTx <- submitTxConstraintsWith @Sale lookups tx
+      void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
+      logInfo @String "NFT bought"
+    _ -> logError @String $ "Sale not founded. Datums: " ++ (show tst)
+
+type NFTSaleSchema =
+  Endpoint "start" StartSaleParams
+    .\/ Endpoint "buy" NFT
+    .\/ Endpoint "close" NFT
+
+saleEndpoints :: Contract () NFTSaleSchema Text ()
+saleEndpoints = selectList [buy, close, start] >> saleEndpoints
+  where
+    buy = endpoint @"buy" buyNFT
+    close = endpoint @"close" closeSale
+    start = endpoint @"start" startSale
---
+++ src/Trace.hs
@@ -0,0 +1,114 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Trace (runTrace) where
+
+import Control.Lens
+import Control.Monad.Freer.Extras as Extras
+import Data.Default
+import Data.Functor (void)
+import qualified Data.Map as Map
+import Ledger
+import Ledger.Ada
+import Ledger.Value
+import NFTSale (NFT (..), StartSaleParams (..), saleEndpoints)
+import Plutus.Contract.Test
+import Plutus.Trace
+
+testTrace :: EmulatorTrace ()
+testTrace = do
+  h1 <- activateContractWallet (knownWallet 1) saleEndpoints
+  h2 <- activateContractWallet (knownWallet 2) saleEndpoints
+
+  callEndpoint @"start" h1 $
+    StartSaleParams
+      { sspPrice = 2_000_000,
+        sspNFT =
+          NFT
+            { nftTokenName = tn,
+              nftCurrencySymbol = cur
+            }
+      }
+  void $ waitNSlots 1
+  callEndpoint @"buy" h2 $
+    NFT
+      { nftTokenName = tn,
+        nftCurrencySymbol = cur
+      }
+  void $ waitNSlots 1
+  Extras.logInfo @String "end"
+
+runTrace :: IO ()
+runTrace = runEmulatorTraceIO' def emuConfBuy testTrace
+
+emuConfBuy :: EmulatorConfig
+emuConfBuy =
+  def & initialChainState
+    .~ ( Left $
+           Map.fromList
+             [ (knownWallet 1, lovelaceValueOf 100_000_000 <> assetClassValue nftToken 1),
+               (knownWallet 2, lovelaceValueOf 10_000_000)
+             ]
+       )
+
+emuConfClose :: EmulatorConfig
+emuConfClose =
+  def & initialChainState
+    .~ ( Left $
+           Map.fromList
+             [ (knownWallet 1, lovelaceValueOf 100_000_000 <> assetClassValue nftToken 1)
+             ]
+       )
+
+tn :: TokenName
+tn = "ABC"
+
+cur :: CurrencySymbol
+cur = "aa"
+
+nftToken :: AssetClass
+nftToken = AssetClass (cur, tn)
+
+closeTrace :: EmulatorTrace ()
+closeTrace = do
+  h1 <- activateContractWallet (knownWallet 1) saleEndpoints
+  callEndpoint @"start" h1 $
+    StartSaleParams
+      { sspPrice = 2_000_000,
+        sspNFT =
+          NFT
+            { nftTokenName = tn,
+              nftCurrencySymbol = cur
+            }
+      }
+  void $ waitNSlots 1
+  callEndpoint @"close" h1 $
+    NFT
+      { nftTokenName = tn,
+        nftCurrencySymbol = cur
+      }
+  void $ waitNSlots 1
+  Extras.logInfo @String "end"
+
+runCloseTrace :: IO ()
+runCloseTrace = runEmulatorTraceIO' def emuConfClose closeTrace
+
+onlyStartTrace :: EmulatorTrace ()
+onlyStartTrace = do
+  h1 <- activateContractWallet (knownWallet 1) saleEndpoints
+  callEndpoint @"start" h1 $
+    StartSaleParams
+      { sspPrice = 2_000_000,
+        sspNFT =
+          NFT
+            { nftTokenName = tn,
+              nftCurrencySymbol = cur
+            }
+      }
+  void $ waitNSlots 1
+  Extras.logInfo @String "end"
+
+runOnlyStartTrace :: IO ()
+runOnlyStartTrace = runEmulatorTraceIO' def emuConfClose onlyStartTrace
